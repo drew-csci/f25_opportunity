4161f8ae8f4f93c118520fe653195979c4dc9b7a
Ayousef8 <Ayousef@drew.edu>
2025-12-16 16:06:51 -0500

    feat: Add buggy search route

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from . import buggy_view   # <-- add this import
 
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('screen1/', views.screen1, name='screen1'),
     path('screen2/', views.screen2, name='screen2'),
     path('screen3/', views.screen3, name='screen3'),
+    path('buggy/', buggy_view.buggy_search, name='buggy_search'),  # <-- add this route
 ]
